2a1e19e92071733f4543a38184998f6feef7bec4
elRubenof <elrubenof@gmail.com>
2024-10-06 23:55:44 +0200

    Updated display name and favicon

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Landex",
       theme: Theme.of(context).copyWith(
         textSelectionTheme: const TextSelectionThemeData(
           selectionColor: Colors.white24,
